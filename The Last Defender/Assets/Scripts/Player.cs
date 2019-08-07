40d50d745c0269bf9c9a434b105e28ead7ae2d7a
Ikuti <m.j.olkiewicz@gmail.com>
2019-08-07 23:08:18 +0200

    Added splash screen, working music player, organized files to the folders, added standard assets, waypoint system and a working movement control from left to right for the ship

---
+++ The Last Defender/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms^-1")][SerializeField] private float xSpeed = 4f;
+
+    [SerializeField] private float xScreenEdgeValue = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x+xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xScreenEdgeValue, xScreenEdgeValue);
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y,transform.localPosition.z);
+    }
+}
