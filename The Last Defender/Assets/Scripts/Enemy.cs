6eeadaa5126dfdd263297086a7cdb8c462c8f32b
Ikuti <m.j.olkiewicz@gmail.com>
2019-08-12 14:52:10 +0200

    Adding non trigger colliders to enemies through code

---
+++ The Last Defender/Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Collider _enemyBoxCollider;
+    private void Awake()
+    {
+        _enemyBoxCollider = gameObject.AddComponent<BoxCollider>();
+        _enemyBoxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        Destroy(gameObject);
+    }
+}
